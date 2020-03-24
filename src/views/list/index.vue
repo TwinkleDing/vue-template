e3220cd1eab119ef8b2f25d572c3cc6371a26912
dingyuliangM <yuliang.ding@maiunsoft.com>
2020-03-24 10:26:49 +0800

    add lists page

--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -1,15 +1,107 @@
 <template>
-  <div>
-    list
+  <div class='pages-lists'>
+    <div class="list-1">
+      <el-table
+        :data="tableData"
+        style="width: 800px"
+        @selection-change="handleSelectionChange">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+          <template slot-scope="scope">
+            <el-tag size="medium">{{ scope.row.name }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+        <el-table-column
+          fixed="right"
+          label="操作"
+          width="100">
+          <template slot-scope="scope">
+            <el-button @click="handleClick(scope.row)"
+                        type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pages.currentPage"
+        :page-sizes="[10, 20, 30, 50]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="pages.total">
+      </el-pagination>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Lists'
+  name: 'Lists',
+  data() {
+    return {
+      pages: {
+        currentPage:1,
+        total: 100
+      },
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    };
+  },
+  methods: {
+    handleClick() {
+
+    },
+    handleSelectionChange() {
+
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  }
 };
 </script>
 
 <style lang="less">
-
+.pages-lists{
+  height: auto;
+  padding-bottom: 20px;
+}
+.list-1{
+  background: #fff;
+  width: 800px;
+}
 </style>
