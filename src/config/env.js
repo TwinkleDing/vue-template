a4ccb1b15b369acf7edf6022b09a6bd109aff32e
TwinkleDing <32945061+TwinkleDing@users.noreply.github.com>
2021-07-04 16:14:50 +0800

    update

--- src/config/env.js
+++ src/config/env.js
@@ -4,6 +4,6 @@ const env = process.env;
 if (env.NODE_ENV === 'development') {
   baseUrl = 'http://localhost:3333/api';
 } else if (env.NODE_ENV === 'production') {
-  baseUrl = 'http://192.168.1.4:666';
+  baseUrl = 'http://localhost:3333/api';
 }
 export { baseUrl, env };
